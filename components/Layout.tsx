8593033dd48962009dbef85f347fe60c15e77fd3
Tanachot <TanachotNew2543@gmail.com>
2023-03-21 15:01:46 +0700

    First Init

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import { AppProps } from "next/app";
+import Footer from "./Footer";
+import Navbar from "./Nav";
+import { ReactNode } from 'react'
+
+type LayoutProps = {
+    children: ReactNode
+    pageProps: any // Or, define a type for pageProps
+  }
+
+export default function Layout({ children, pageProps }: LayoutProps) {
+    return (
+        <>
+            <Navbar></Navbar>
+                {children}
+            <Footer></Footer>
+        </>
+    );
+}
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
     return (
         <>
-            <h1>body</h1>
+            <h1>All product</h1>
         </>
     );
 };
