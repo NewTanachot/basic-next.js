07e5eb603c2ab7e9ce29f9dade38ef506c85bea4
Tanachot <TanachotNew2543@gmail.com>
2023-05-01 15:31:56 +0700

    remove bootstrrap 5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        {/* Import Bootstrap 5 */}
+        {/* Import Bootstrap 5
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous"></link>
         </Head>
 
@@ -22,8 +22,9 @@ export default function App({ Component, pageProps }: AppProps) {
             integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
             crossOrigin="anonymous"
             strategy="beforeInteractive"
-        />
+        /> */}
         
+        </Head>
         <Layout pageProps={pageProps}>
             <Component {...pageProps} />
         </Layout>
